81bd7383b20dd488478d05473f7698bda6e20e89
Ana Marlen Aguilar <ana.aguilar@improving.com>
2022-05-26 15:31:01 -0500

    build: angular-button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
+import 'zone.js';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  entryComponents: [AppComponent],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+  ngDoBootstrap() {
+    const el = createCustomElement(AppComponent, {
+      injector: this.injector,
+    });
+    customElements.define('angular-button', el);
+  }
+}
---
+++ src/bootstrap.ts
@@ -0,0 +1,19 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+  const element: HTMLElement = document.createElement('angular-button');
+
+
+export const component = (): HTMLElement => {
+  return element;
+};
