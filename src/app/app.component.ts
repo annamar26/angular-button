ef7db60e4c42f2dc931f1efc5841175306e12aa7
Emma Aguila <Emma.Ramirez@improving.com>
2022-05-31 11:53:47 -0500

    build:add custom event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-button';
+  counterAngular: number = 0;
+  addClick(): void {
+    this.counterAngular++;
+    const angularEvent = new CustomEvent('clickAngular', {
+      bubbles: true,
+      cancelable: true,
+      composed: true,
+    });
+    window.dispatchEvent(angularEvent);
+  }
 }
