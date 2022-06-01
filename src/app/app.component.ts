0ced94fc5b2297b2801255e514d96eb83d5b9266
Emma Aguila <Emma.Ramirez@improving.com>
2022-06-01 15:44:09 -0500

    refactor: handle state with event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent{
-  @Input() color!: string;
+export class AppComponent implements OnInit {
+  color!: string;
+  bgColor!: string;
   title = 'angular-button';
   counterAngular: number = 0;
+
+  ngOnInit(): void {
+    window.addEventListener('changeColor5', (event: CustomEventInit) => {
+      this.color = event.detail.color;
+      this.bgColor = event.detail.bgColor;
+      console.log(event);
+    });
+    window.addEventListener('changeColor10', (event: CustomEventInit) => {
+      this.color = event.detail.color;
+      this.bgColor = event.detail.bgColor;
+    });
+  }
+
   addClick(): void {
     this.counterAngular++;
     const angularEvent = new CustomEvent('clickAngular', {
